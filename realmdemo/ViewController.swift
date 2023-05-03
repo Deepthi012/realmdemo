a4d8430ecd1980e12f70789a1567a7adcd25c131
Deepthi <deepthi.s@westagilelabs.com>
2023-05-03 15:30:56 +0530

    Initial Commit

---
+++ realmdemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realmdemo
+//
+//  Created by West Agile Labs on 03/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
